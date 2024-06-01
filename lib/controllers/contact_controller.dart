2d0f8e295d1761423620a9ae78c3c0abd19f57bf
Teerasej <teerasej@gmail.com>
2024-06-01 16:38:38 +0700

    create table on init database with method

--- lib/controllers/contact_controller.dart
+++ lib/controllers/contact_controller.dart
@@ -1,10 +1,28 @@
 import 'package:contact_app/models/contact_model.dart';
 import 'package:get/get.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
 
 class ContactController extends GetxController {
   final contacts = <ContactModel>[].obs;
+  Database? _database;
 
   void addContact(ContactModel contact) {
     contacts.add(contact);
+  }
+
+  Future<void> _initDatabase() async {
+    final directory = await getApplicationDocumentsDirectory();
+    final path = join(directory.path, 'contacts.db');
+    _database = await openDatabase(
+      path,
+      version: 1,
+      onCreate: (db, version) {
+        return db.execute(
+          'CREATE TABLE contacts(name TEXT, email TEXT)',
+        );
+      },
+    );
   }
 }
